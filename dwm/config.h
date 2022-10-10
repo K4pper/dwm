3bc9c38ee8045afdf80814defb97d30d606efdac
Kasper Therkelsen <kath@ok.dk>
2022-10-10 19:26:25 +0200

    added vscode shortcut

--- dwm/config.h
+++ dwm/config.h
@@ -61,12 +61,14 @@ static const Layout layouts[] = {
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
+static const char *vscodecmd[]  = { "code", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,				        XK_t, 	   spawn,          {.v = termcmd } },
 	{ MODKEY,				        XK_b, 	   spawn,          {.v = browsercmd } },
+	{ MODKEY,				        XK_c, 	   spawn,          {.v = vscodecmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
